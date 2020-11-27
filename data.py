8a1b6a5ff996f2f0c5f689d9065ee42223409f82
CodeChrisB <c.buchberger01@gmail.com>
2020-11-27 12:44:19 +0100

    removed output to make it 40% faster

--- data.py
+++ data.py
@@ -7,17 +7,17 @@
 # Keep in mind that the quotation marks --> "" are 
 # necessary, and should not be removed.
 #If you only want 2 answers leave the others blank --> ""
-YourQuestion = "Do you love me?"
-FirstAnswer ="Yes"
-SecondAnswer ="No"
-ThridAnswer ="Maybe"
-FourthAnswer ="A bit"
-FifthAnswer ="Love? Your my soulmate"
-SixthAnswer ="No I'd rather die"
-SeventhAnswer = "#Friendzone"
-EighthAnswer = "Bro,no"
+YourQuestion = "Wer ist die gröẞere Bitch"
+FirstAnswer ="Max"
+SecondAnswer ="Lisa"
+ThridAnswer =""
+FourthAnswer =""
+FifthAnswer =""
+SixthAnswer =""
+SeventhAnswer = ""
+EighthAnswer = ""
 NinthAnswer = ""
-BulkNumber =1000 # How often do you want to send your question?
+BulkNumber =1000000000 # How often do you want to send your question?
 
 
 
@@ -38,6 +38,7 @@ class Data:
             "options-option-7": SeventhAnswer,
             "options-option-8": EighthAnswer,
             "options-option-9": NinthAnswer,
+            "f154f5e7fac7ead116b8382d45a2fe8d9": "y",
             "poll-submit": "create"
         }
         return body
--- strawBot.py
+++ strawBot.py
@@ -1,4 +1,47 @@
+#
 #######################
+# Straw Poll Bulk Bot #
+# @CodeChrisB         #
+#######################
+# You can change the Questions and Answers underneath
+# Keep in mind that the quotation marks --> "" are 
+# necessary, and should not be removed.
+#If you only want 2 answers leave the others blank --> ""
+YourQuestion = "How many Numbers are there?"
+FirstAnswer ="Infinite"
+SecondAnswer ="None at all"
+ThridAnswer ="42"
+FourthAnswer =""
+FifthAnswer =""
+SixthAnswer =""
+SeventhAnswer = ""
+EighthAnswer = ""
+NinthAnswer = ""
+BulkNumber =1000000000 # How often do you want to send your question?
+
+
+
+###########################
+#      Do not change      #
+#  the values underneath. #
+###########################
+body = {
+    "poll-title": YourQuestion,
+    "options-option-1": FirstAnswer,
+    "options-option-2": SecondAnswer,
+    "options-option-3":ThridAnswer,
+    "options-option-4": FourthAnswer,
+    "options-option-5": FifthAnswer,
+    "options-option-6": SixthAnswer,
+    "options-option-7": SeventhAnswer,
+    "options-option-8": EighthAnswer,
+    "options-option-9": NinthAnswer,
+    "f154f5e7fac7ead116b8382d45a2fe8d9": "y",
+    "poll-submit": "create"
+}
+
+         
+# ######################
 # Straw Poll Bulk Bot #
 # @CodeChrisB         #
 #######################
@@ -18,48 +61,17 @@
 import requests
 import concurrent.futures
 from time import sleep
-from data import Data,BulkNumber
 
-
+print('Start Bot')
 
 #This is the url of strawpoll
 url = 'https://www.strawpoll.me/'
 
-#Here we get the data that we have defined in
-#data.py (You can change it in data.py)
-data = Data()
-body =data
-
-
-#This is how many 'cool' Id's we got in this run
-coolNumber =0
-
 # We define here the Function that sends 
 # a post request to Strawpoll.me
 def post_poll():
-    r = requests.post(url, data=body)
+    requests.post(url, data=body)
 
-    #For a ~4% performance boost you can remove the print(r.text) underneath here
-    print('https://strawpoll.me/'+r.text[29:36])
-    # this maybe looks weird, but somehow we have to
-    # check the poll Number and decide if we want to
-    # save  the  id into  data.py  (the text file we 
-    # want to save special Poll Numbers
-    if  r.text.count('0000000') >= 1 or r.text.count('1111111') >= 1 or r.text.count('2222222') >= 1 or r.text.count('3333333') >= 1 or r.text.count('4444444') >= 1 or r.text.count('5555555') >= 1 or r.text.count('6666666') >= 1 or r.text.count('777777') >= 1 or r.text.count('888888') >= 1 or r.text.count('999999') >= 1:
-        print(r.text)
-        f = open("requests.txt", "a")
-        f.write(r.text+'\n')
-        f.close()
-        coolNumber+=1
-
-
-print('I will create : ' +str(BulkNumber) + ' Polls')
-sleep(2)
-# Here we use Mulithreading to send the post requests
-# as fast as possible
 with concurrent.futures.ThreadPoolExecutor() as executor:
     for i in range(BulkNumber):
         executor.submit(post_poll)
-
-print('I\'m done with creating ' +str(BulkNumber) + ' Polls')
-print('I have saved ' +str(coolNumber) +' cool ID\'s')
